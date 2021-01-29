dde24e0463bf78fb27df20b4f80d78087deec240
Ike <ikescode@gmail.com>
2021-01-28 20:07:14 -0600

    parse template

--- makesite.go
+++ makesite.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
+	"text/template"
 )
 
-func main() {
-	bytesToWrite := []byte("hello\ngo\n")
-	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
+func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	check(err)
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	err = t.Execute(os.Stdout, string(fileContents))
+	check(err)
 }
