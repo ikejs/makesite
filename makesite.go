8abbd8debbbf84d5bfb93a5434004e9c492b8698
Ike <ikescode@gmail.com>
2021-02-02 19:19:56 -0600

    add file flag, add comments

--- makesite.go
+++ makesite.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
+	"html/template"
 	"io/ioutil"
 	"os"
-	"text/template"
 )
 
 func check(err error) {
@@ -13,11 +14,19 @@ func check(err error) {
 }
 
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+	// get path of file
+	fileName := flag.String("file", "latest-post.txt", "Path of the .txt file.")
+	// parse input text
+	flag.Parse()
+	// read file at fileName path, set contents to fileContents
+	fileContents, err := ioutil.ReadFile(*fileName)
 	check(err)
+	// create new HTML file in system
 	newFile, err := os.Create("first-post.html")
 	check(err)
+	// create new template
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	// write to HTML file
 	err = t.Execute(newFile, string(fileContents))
 	check(err)
 }
