c2d4ba838571a871061df70554e0efd206773547
Ike <ikescode@gmail.com>
2021-02-02 19:47:31 -0600

    generate html files for all txt files

--- makesite.go
+++ makesite.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func check(err error) {
@@ -14,19 +15,26 @@ func check(err error) {
 }
 
 func main() {
-	// get path of file
-	fileName := flag.String("file", "latest-post.txt", "Path of the .txt file.")
-	// parse input text
-	flag.Parse()
-	// read file at fileName path, set contents to fileContents
-	fileContents, err := ioutil.ReadFile(*fileName)
+	// get path of dir with posts
+	dirName := flag.String("dir", ".", "Path to directory with posts.")
+	// get all files from dirName
+	files, err := ioutil.ReadDir(*dirName)
 	check(err)
-	// create new HTML file in system
-	newFile, err := os.Create("first-post.html")
-	check(err)
-	// create new template
-	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	// write to HTML file
-	err = t.Execute(newFile, string(fileContents))
-	check(err)
+	// loop over files
+	for _, file := range files {
+		// check if file is dir
+		if !file.IsDir() && filepath.Ext(file.Name()) == ".txt" {
+			// get file path, set contents to fileContents
+			fileContents, err := ioutil.ReadFile(file.Name())
+			check(err)
+			// create new HTML file in system
+			newFile, err := os.Create(file.Name() + ".html")
+			check(err)
+			// create new template
+			t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+			// write to HTML file
+			err = t.Execute(newFile, string(fileContents))
+			check(err)
+		}
+	}
 }
