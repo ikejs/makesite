054eb0519b8ddb6a700c5f26ffc7b51ae4fc2157
Ike <ikescode@gmail.com>
2021-02-02 18:42:22 -0600

    write HTML template to system

--- makesite.go
+++ makesite.go
@@ -15,7 +15,9 @@ func check(err error) {
 func main() {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	check(err)
+	newFile, err := os.Create("first-post.html")
+	check(err)
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	err = t.Execute(os.Stdout, string(fileContents))
+	err = t.Execute(newFile, string(fileContents))
 	check(err)
 }
