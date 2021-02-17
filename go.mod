cf872f88f22824659f83a64b7558f8b83a2c6355
Ike <ikescode@gmail.com>
2021-02-16 21:59:21 -0600

    add sass compiler

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module makesite
 
 go 1.13
+
+require (
+	github.com/wellington/go-libsass v0.9.2
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+)
--- makesite.go
+++ makesite.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	libsass "github.com/wellington/go-libsass"
 )
 
 func check(err error) {
@@ -23,18 +25,47 @@ func main() {
 	// loop over files
 	for _, file := range files {
 		// check if file is dir
-		if !file.IsDir() && filepath.Ext(file.Name()) == ".txt" {
+		if !file.IsDir() {
 			// get file path, set contents to fileContents
 			fileContents, err := ioutil.ReadFile(file.Name())
 			check(err)
-			// create new HTML file in system
-			newFile, err := os.Create(file.Name() + ".html")
-			check(err)
-			// create new template
-			t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-			// write to HTML file
-			err = t.Execute(newFile, string(fileContents))
-			check(err)
+			if filepath.Ext(file.Name()) == ".txt" {
+				// create new HTML file in system
+				newFile, err := os.Create(file.Name() + ".html")
+				check(err)
+				// create new template
+				t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+				// write to HTML file
+				err = t.Execute(newFile, string(fileContents))
+				check(err)
+			}
+			if filepath.Ext(file.Name()) == ".scss" {
+				// open sass file
+				sassFile, err := os.Open("test.scss")
+				if err != nil {
+					panic(err)
+				}
+				defer sassFile.Close()
+
+				// create new css file
+				outputFile, err := os.Create("styles.css")
+				if err != nil {
+					panic(err)
+				}
+				defer outputFile.Close()
+
+				// instantiate libsass
+				cssCompiler, err := libsass.New(outputFile, sassFile)
+				if err != nil {
+					panic(err)
+				}
+
+				// compile css
+				if err := cssCompiler.Run(); err != nil {
+					panic(err)
+				}
+			}
 		}
+
 	}
 }
